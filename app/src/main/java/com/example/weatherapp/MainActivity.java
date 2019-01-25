781c62bfe216749c269e542c6c4c511e8b4a807f
Rizi <rizwanahmed.r920@gmail.com>
2019-01-25 21:02:52 +0800

    Tested, Exceptions handled: no internet connection, location not found, more than one time getting forecast, empty edit

--- app/src/main/java/com/example/weatherapp/MainActivity.java
+++ app/src/main/java/com/example/weatherapp/MainActivity.java
@@ -1,8 +1,10 @@
 package com.example.weatherapp;
 
+import android.content.Context;
 import android.databinding.DataBindingUtil;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.net.ConnectivityManager;
 import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -23,9 +25,11 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
+import java.net.InetAddress;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLConnection;
+import java.net.UnknownHostException;
 
 public class MainActivity extends AppCompatActivity {
     String tag = "LifeCycleEvents";
@@ -36,27 +40,23 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
 
 
+    public void btnOnClick(View view){
         final EditText latitude = (EditText) findViewById(R.id.etLatitude);
         final EditText longitude = (EditText) findViewById(R.id.etLongitude);
-        Button button = (Button) findViewById(R.id.btnForecast);
-
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-                // accept longitude and latitudes from User
-                String lat = String.valueOf(latitude.getText());
-                String lon = String.valueOf(longitude.getText());
-                // Open weather API url
-                String URL1 = "http://api.openweathermap.org/data/2.5/weather?lat="+lat+"&lon="+lon+"&APPID=4f6f268a496ed32149b37570e57bef91";
-                new GetWeatherData().execute(URL1);
-
-            }
-        });
-
-
+        if(!isNetworkAvailable(getApplicationContext())==true){
+            Toast.makeText(getApplicationContext(), "No Internet, Please connect to a Network", Toast.LENGTH_LONG).show();
+        }
+        String lat = String.valueOf(latitude.getText());
+        String lon = String.valueOf(longitude.getText());
+        if(lat.equalsIgnoreCase("")||lon.equalsIgnoreCase("")){
+            Toast.makeText(getApplicationContext(), "Please Enter latitude and longitude to get the forecast!", Toast.LENGTH_LONG).show();
+        }else {
+            String URL1 = "http://api.openweathermap.org/data/2.5/weather?lat=" + lat + "&lon=" + lon + "&APPID=4f6f268a496ed32149b37570e57bef91";
+            new GetWeatherData().execute(URL1);
+        }
     }
 
 
@@ -75,59 +75,59 @@ public class MainActivity extends AppCompatActivity {
                 InputStreamReader reader = new InputStreamReader(in);
 
                 int data = reader.read();
-
                 while (data != -1){
-
                     char current = (char) data;
                     result += current;
                     data = reader.read();
                 }
-
-                Log.d("result: ","------------------------>"+result);
                 return result;
-
+                
             } catch (MalformedURLException e) {
-
                 e.printStackTrace();
             } catch (IOException e) {
-
                 e.printStackTrace();
             }
 
             return null;
     }
 
-        protected void onPostExecute(String result){
-            try{
+        protected void onPostExecute(String result) {
 
-                JSONObject jsonObject =new JSONObject(result);
-            JSONArray jsonArray = jsonObject.getJSONArray("weather");
-            String iconUrl=null;
-            String desc=null;
-            String description;
-            for (int i = 0; i < jsonArray.length(); i++)
-            {
-                iconUrl = jsonArray.getJSONObject(i).getString("icon");
-                 desc = jsonArray.getJSONObject(i).getString("description");
-                Log.e("icon_url:------->",iconUrl);
+
+            if (result==null || result.equalsIgnoreCase("")) {
+                Toast.makeText(getApplicationContext(), "Sorry! No such location found", Toast.LENGTH_LONG).show();
+            } else {
+                try {
+                    JSONObject jsonObject = new JSONObject(result);
+                    JSONArray jsonArray = jsonObject.getJSONArray("weather");
+                    String iconUrl = null;
+                    String desc = null;
+                    String description;
+                    for (int i = 0; i < jsonArray.length(); i++) {
+                        iconUrl = jsonArray.getJSONObject(i).getString("icon");
+                        desc = jsonArray.getJSONObject(i).getString("description");
+
+                    }
+                    char[] chars = desc.toCharArray();
+                    chars[0] = Character.toUpperCase(chars[0]);
+                    description = new String(chars);
+                    String imgUrl = "http://openweathermap.org/img/w/" + iconUrl + ".png";
+                    float tempMin = (float) (Float.valueOf(jsonObject.getJSONObject("main").getString("temp_min")) - 272.15);
+                    float tempMax = (float) (Float.valueOf(jsonObject.getJSONObject("main").getString("temp_max")) - 272.15);
+                    float temp = (float) (Float.valueOf(jsonObject.getJSONObject("main").getString("temp")) - 272.15);
+
+                    new DownloadImageTask().execute(imgUrl);
+                    activityMainBinding = DataBindingUtil.setContentView(MainActivity.this, R.layout.activity_main);
+                    WeatherInfo obj = new WeatherInfo(description + " (" + temp + DEGREE + "C)", "Min: " + String.valueOf(tempMin) + DEGREE + "C", "Max: " + String.valueOf(tempMax) + DEGREE + "C");
+                    activityMainBinding.setWeatherInfo(obj);
+
+                } catch (Exception x) {
+                    x.printStackTrace();
+                }
+
             }
-                char[] chars = desc.toCharArray();
-                chars[0] = Character.toUpperCase(chars[0]);
-                description= new String(chars);
-                String imgUrl="http://openweathermap.org/img/w/"+iconUrl+".png";
-                float tempMin=(float)(Float.valueOf(jsonObject.getJSONObject("main").getString("temp_min"))-272.15);
-                float tempMax=(float)(Float.valueOf(jsonObject.getJSONObject("main").getString("temp_max"))-272.15);
-                float temp=(float)(Float.valueOf(jsonObject.getJSONObject("main").getString("temp"))-272.15);
+        }
 
-                new DownloadImageTask().execute(imgUrl);
-                activityMainBinding= DataBindingUtil.setContentView(MainActivity.this,R.layout.activity_main);
-                WeatherInfo obj=new WeatherInfo(description+" ("+temp+DEGREE+"C)", "Min: "+String.valueOf(tempMin)+DEGREE+"C","Max: "+String.valueOf(tempMax)+DEGREE+"C");
-                activityMainBinding.setWeatherInfo(obj);
-
-            }catch (Exception x){
-                x.printStackTrace();
-            }
-    }
     }
 
 
@@ -162,10 +162,18 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+    public boolean isNetworkAvailable(Context context) {
+        final ConnectivityManager connectivityManager = ((ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE));
+        return connectivityManager.getActiveNetworkInfo() != null && connectivityManager.getActiveNetworkInfo().isConnected();
+    }
+
     @Override
     public void onStart()
     {
         super.onStart();
+        if(!isNetworkAvailable(getApplicationContext())==true){
+            Toast.makeText(getApplicationContext(), "No Internet, Please connect to a Network", Toast.LENGTH_LONG).show();
+        }
         Log.d(tag, "In the onStart() event");
     }
 
