4afa578cf28e17a5616aa2c6510896d6c1276874
Rizi <rizwanahmed.r920@gmail.com>
2019-01-25 16:58:09 +0800

    Network Security configurations Done

--- app/src/main/java/com/example/weatherapp/MainActivity.java
+++ app/src/main/java/com/example/weatherapp/MainActivity.java
@@ -1,12 +1,24 @@
 package com.example.weatherapp;
 
 import android.databinding.DataBindingUtil;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 
 import com.example.weatherapp.databinding.ActivityMainBinding;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
 
 public class MainActivity extends AppCompatActivity {
     String tag = "LifeCycleEvents";
@@ -22,6 +34,62 @@ public class MainActivity extends AppCompatActivity {
 
         final EditText latitude = (EditText) findViewById(R.id.etLatitude);
         final EditText longitude = (EditText) findViewById(R.id.etLongitude);
+        Button button = (Button) findViewById(R.id.btnForecast);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                // accept longitude and latitudes from User
+                String lat = String.valueOf(latitude.getText());
+                String lon = String.valueOf(longitude.getText());
+                // Open weather API url
+                String URL1 = "http://api.openweathermap.org/data/2.5/weather?lat="+lat+"&lon="+lon+"&APPID=4f6f268a496ed32149b37570e57bef91";
+                new GetWeatherData().execute(URL1);
+
+            }
+        });
+
+
+    }
+
+
+    private class GetWeatherData extends AsyncTask<String,Void,String>{
+
+        @Override
+        protected String doInBackground(String... urls) {
+            String result = "";
+            URL url;
+            HttpURLConnection urlConnection = null;
+
+            try {
+                url = new URL(urls[0]);
+                urlConnection =  (HttpURLConnection) url.openConnection();
+                InputStream in = urlConnection.getInputStream();
+                InputStreamReader reader = new InputStreamReader(in);
+
+                int data = reader.read();
+
+                while (data != -1){
+
+                    char current = (char) data;
+                    result += current;
+                    data = reader.read();
+                }
+
+                Log.d("result: ","------------------------>"+result);
+                return result;
+
+
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            return null;
+    }
+
     }
 
     @Override
